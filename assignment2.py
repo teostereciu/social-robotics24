db5003b7bf5a9390127d1a4afffd5fdaad04af84
Teo Stereciu <104054380+teostereciu@users.noreply.github.com>
2024-05-16 00:37:29 +0200

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -10,17 +10,7 @@ from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.util import sleep
 from dialogue import lines, applause_on_keyword, is_smart_question, possible_answers
 
-#define keywords
-#these keywords are for the break
-fun_keywords = ["tell me", "fact", "tell", "fun"]
-dance_keywords = ["dance", "moves", "show me", "groove", "dense", "show"]
-
-#these keywords are for whether to continue
-positive_keywords = ["yes", "ja", "yea", "sure", "of course"]
-negative_keywords = ["no", "nope", "nah"]
-
-#define on keyword function
-#this function indicates the response for each keyword question
+# indicates the response to keywords
 @inlineCallbacks
 def on_keyword(frame):
     c = frame["data"]["body"]["certainty"]
@@ -29,22 +19,20 @@ def on_keyword(frame):
             frame["data"]["body"]["certainty"] > 0.45):
         
         if frame["data"]["body"]["text"] in ['clap']:
+            yield sess.call("rie.dialogue.say", text="Yes! Let's clap together.") 
             sess.call("rom.optional.behavior.play", name="BlocklyApplause")
-        elif frame["data"]["body"]["text"] in ['bye', 'stop', 'end']:
-            sess.call("rie.dialogue.say",
-                      test="Alright. Bye!")
-            sess.call("rom.optional.behavior.play", name="BlocklyWaveRightArm")
-            sess.leave()
-
+        
+# indicates the response to touches
 @inlineCallbacks
 def touched(frame):
+    # head touch prompts the session to end
     if ("body.head.front" in frame["data"] or
         "body.head.middle" in frame["data"] or
             "body.head.rear" in frame["data"]):
         print("Why touching my head?")
         yield sess.call("rie.dialogue.say",
                         text="That was it for today. Bye for now! ")
-
+        sess.leave()
 
 
 @inlineCallbacks
@@ -54,42 +42,38 @@ def main(session, details):
 
     yield sess.call("rom.optional.behavior.play", name="BlocklyStand")
 
+    # look for a face, say hello and wave when found
     yield sess.call("rie.vision.face.find")
     line = lines[0]
-    yield session.call("rie.dialogue.say", text=line)  # say hello
+    yield sess.call("rie.dialogue.say", text=line)
     yield sess.call("rom.optional.behavior.play", name="BlocklyWaveRightArm")
+
+    # introduce myself and lesson topic
     line = lines[1]
-    yield session.call("rie.dialogue.say", text=line) # introduce myself
+    yield session.call("rie.dialogue.say", text=line) 
 
-
-    # introduce volcanoes
-    #question = lines[1]
-    #answers = {"yes": ["yes", "yeah", "sure", "yup", "yay"], "no": ["no", "nah", "nope", "nay"]}
-
-    '''line = "Today we will dive into the fiery world of volcanoes. " + lines[4]
+    line = lines[2]
     yield session.call("rie.dialogue.say", text=line)
 
     # parts of a volcano
-    line = lines[5]
+    line = lines[3]
     yield session.call("rie.dialogue.say", text=line)
 
     # magma
-    line = lines[6]
+    line = lines[4]
     yield session.call("rie.dialogue.say", text=line)
 
     # vent
-    line = lines[7]
+    line = lines[5]
     yield session.call("rie.dialogue.say", text=line)
 
     # crater
-    line = lines[8]
+    line = lines[6]
     yield session.call("rie.dialogue.say", text=line)
-    '''
+    
     # erruption
-    line = lines[9]
+    line = lines[7]
     yield session.call("rie.dialogue.say", text=line)
-    line = lines[10]
-    #yield session.call("rie.dialogue.say", text=line)
     
     # custom gesture
     yield sess.call("rom.actuator.motor.write", frames=[{"time": 200, "data": {"body.arms.right.lower.roll": -1.5,
@@ -107,71 +91,69 @@ def main(session, details):
                                                                                "body.arms.right.upper.pitch": -2.5,
                                                                                "body.arms.left.upper.pitch": -2.5}
                                                          }], force=True)
-    line = lines[10]
-    yield session.call("rie.dialogue.say", text=line) #eruption
+    line = lines[8]
+    yield session.call("rie.dialogue.say", text=line) 
     yield sess.call("rom.optional.behavior.play", name="BlocklyStand")
-
-    '''
+    # end eruption
+    
+    # explain eruption
+    line = lines[9]
+    yield session.call("rie.dialogue.say", text=line) 
+    
     # lava
-    line = lines[11]
+    line = lines[10]
     yield session.call("rie.dialogue.say", text=line)
 
     # ashes
-    line = lines[12]
-    yield session.call("rie.dialogue.say", text=line)'''
+    line = lines[11]
+    yield session.call("rie.dialogue.say", text=line)
 
     # start quiz with aruco cards
     # ash question
-    '''line = lines[13]
+    line = lines[12]
     yield session.call("rie.dialogue.say", text=line)
-    # wait until we see a card
     correct = False
     cards = ['volcano', 'magma', 'vent', 'crater', 'lava', 'ash']
     while not correct:
-        frames = yield session.call("rie.vision.card.read")
-        print(frames[0])
+        frames = yield session.call("rie.vision.card.read")  # wait until i see a card
         card_id = frames[0]['data']['body'][0][-1]
-        print(frames[0]['data']['body'])
         if cards[card_id] == 'ash':
             correct = True
         else:
             # wrong, try again
-            line = lines[14]
+            line = lines[13] # optionally replace '<>' with what the student said, so cards[card_id]
             yield session.call("rie.dialogue.say", text=line)
     
     # correct, move on to vent question
-    line = lines[15]
+    line = lines[14]
     yield session.call("rie.dialogue.say", text=line)
     correct = False
     while not correct:
         frames = yield session.call("rie.vision.card.read")
-        print(frames[0])
         card_id = frames[0]['data']['body'][0][-1]
-        print(frames[0]['data']['body'])
         if cards[card_id] == 'vent':
             correct = True
         else:
             # wrong, try again
-            line = lines[16]
+            line = lines[15] # optionally replace '<>' with what the student said, so cards[card_id]
             yield session.call("rie.dialogue.say", text=line)
    
-    # correct, move on to lava question
-    line = lines[17]
+    # correct, move on to magma question
+    line = lines[16]
     yield session.call("rie.dialogue.say", text=line)
     correct = False
     while not correct:
         frames = yield session.call("rie.vision.card.read")
-        print(frames[0])
         card_id = frames[0]['data']['body'][0][-1]
-        if cards[card_id] == 'vent':
+        if cards[card_id] == 'magma':
             correct = True
         else:
             # wrong, try again
-            line = lines[18]
+            line = lines[17] # optionally replace '<>' with what the student said, so cards[card_id]
             yield session.call("rie.dialogue.say", text=line)
 
     # correct, clap if prompted
-    line = lines[19]
+    line = lines[18]
     yield session.call("rie.dialogue.say", text=line)
     yield session.call("rie.dialogue.config.language", lang="en")
     yield session.call("rie.dialogue.keyword.language", lang="en")
@@ -181,21 +163,18 @@ def main(session, details):
                             "rie.dialogue.keyword.stream")
     yield session.call("rie.dialogue.keyword.stream")
     yield sleep(7)
-    yield session.call("rie.dialogue.say", text="Yes! Let's clap together.")
     yield session.call("rie.dialogue.keyword.clear")
     yield session.call("rie.dialogue.keyword.close")
 
     # end of the lesson
     yield sess.subscribe(touched, "rom.sensor.touch.stream")
     yield sess.call("rom.sensor.touch.stream")
-    sess.call("rom.optional.behavior.play", name="BlocklyWaveRightArm")'''
+    # maybe metion that the student can end the lesson at any time and add this somewhere at the beginning? or make the bot sing at the end of the lesson until it's stopped or sth lol
+
+    sess.leave()  # close the connection with the robot
 
 
-
-    session.leave()  #close the connection with the robot
-
-
-#create wamp connection
+# create wamp connection
 wamp = Component(
     transports=[{
         "url": "ws://wamp.robotsindeklas.nl",
